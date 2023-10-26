ee537165b31d700dbc1278980c92d54e3c98475e
onk108 <wkdtnstjr88@naver.com>
2023-10-26 17:02:37 +0900

    step 2, input

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -2,21 +2,26 @@ package com.ll;
 
 import java.util.Scanner;
 
-class App {
+public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
-            if(cmd.equals("종료")) {
+            if (cmd.equals("종료")) {
                 break;
-            }
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
 
-            System.out.printf("입력받은 명령어 : %s\n", cmd);
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+            }
         }
     }
 }
